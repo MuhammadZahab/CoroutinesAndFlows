04eddb511266f31022cec1bf5bb30dc6367cd083
MuhammadZahabAhmad <zahab.ahmad@jeeny.me>
2025-10-03 09:30:05 +0500

    Assignemnt

--- app/src/main/java/com/zahab/coroutinesandflows/MainActivity.kt
+++ app/src/main/java/com/zahab/coroutinesandflows/MainActivity.kt
@@ -1,48 +1,29 @@
 package com.zahab.coroutinesandflows
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
-import com.zahab.coroutinesandflows.sections.compose_coroutines.CounterScreen
 import com.zahab.coroutinesandflows.sections.compose_coroutines.ProfileScreen
 import com.zahab.coroutinesandflows.ui.theme.CoroutinesAndFlowsTheme
-import kotlinx.coroutines.async
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import kotlin.system.measureTimeMillis
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-//        lifecycleScope.launch {
-//            val timeinmillis = measureTimeMillis {
-//            val innerjob1 = async {
-//                delay(2000L)
-//                println("Inner Job 1 Finished")
-//
-//            }.await()
-//
-//            val innerJob2 = async {
-//                delay(1000L)
-//                println("Inner Job 2 Finished")
-//            }.await()
-//            }
-//
-//            println("Both jobs took $timeinmillis time")
-//        }
-
+      assignment1()
 
         setContent {
             CoroutinesAndFlowsTheme {
@@ -53,6 +34,42 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    fun assignment1() {
+        val mainThread = lifecycleScope.launch {
+
+            launch {
+                while (isActive) {
+                    println("Coo")
+                    delay(1000L)
+                }
+            }
+
+            launch {
+                while (isActive) {
+                    println("Caw")
+
+                    delay(2000L)
+                }
+            }
+
+            launch {
+                while (isActive) {
+                    println("Chirp")
+                    delay(3000L)
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            delay(10000L)
+            println("Cancelling all coroutines")
+            mainThread.cancel()
+        }
+    }
+
+
+
 }
 
 @Composable
@@ -70,3 +87,4 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
